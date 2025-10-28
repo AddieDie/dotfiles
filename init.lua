0a85d6a36ece05d0e36a9dab731c448adf8f5c02
Addie <146877894+AddieDie@users.noreply.github.com>
2025-10-28 17:38:36 +0300

    Add C-Lang/Runner Shortcut

--- init.lua
+++ init.lua
@@ -1,67 +1,70 @@
--- Line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Tabs & indentation
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- Searching
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.hlsearch = true
 
--- UI
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.wrap = false
 vim.opt.scrolloff = 8
 
--- System clipboard
 vim.opt.clipboard = "unnamedplus"
 
--- Leader key
 vim.g.mapleader = " "
-
-
--- Keymaps | ADDIE: ALSO YOU CAN CREATE A KEYMAPS.LUA FILE AND PUT ALL YOUR KEYMAPS IN THERE IF YOU WANT
 vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
--- ADDIE: QUIT ALL KEYMAPS FOR MACOS AND LINUX
 vim.keymap.set("n", "<D-q>", ":qa!<CR>", { noremap = true, silent = true, desc = "Quit all" })
-vim.keymap.set("n", "<A-q>", ":qa!<CR>", { noremap = true, silent = true, desc = "Quit all" })
+
+-- Shortcut for c files | Also works for windows the shortcut is F5
+vim.keymap.set('n', '<F5>', function()
+local file = vim.fn.expand('%:p')       
+local filename = vim.fn.expand('%:t:r') 
+local build_dir = vim.fn.getcwd() .. '/build'
+local output = build_dir .. '/' .. filename
+
+-- Little caution this ll create a build file that files don't have = More storage
+if vim.fn.isdirectory(build_dir) == 0 then
+  vim.fn.mkdir(build_dir, 'p')
+  print("📁 Created build directory: " .. build_dir)
+  end
+
+  
+  local cmd = string.format("gcc %s -o %s && %s", file, output, output)
+  vim.cmd('!clear')  
+  vim.cmd('!' .. cmd)
+  end, { noremap = true, silent = false, desc = "Compile & run C file" })
 
 
--- Package Manager: lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
+    "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
+    "--branch=stable", lazypath,
   })
   end
   vim.opt.rtp:prepend(lazypath)
 
   require("lazy").setup({
-    -- ADDIE: YOU CAN PUY ANY THEME YOU LIKE HERE
     {
       "sainnhe/everforest",
       lazy = false,
       priority = 1000,
       config = function()
-      vim.g.everforest_background = "hard"
+      vim.g.everforest_background = "medium"
+      vim.g.everforest_transparent_background = 1
       vim.g.everforest_enable_italic = true
       vim.cmd("colorscheme everforest")
       end
     },
 
-    -- File explorer
     {
       "nvim-tree/nvim-tree.lua",
       dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -71,7 +74,6 @@ if not vim.loop.fs_stat(lazypath) then
       end
     },
 
-    -- Syntax highlighting
     {
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
@@ -84,48 +86,77 @@ if not vim.loop.fs_stat(lazypath) then
       end
     },
 
-    -- LSP 
-  { "neovim/nvim-lspconfig" },
+    { "neovim/nvim-lspconfig" },
 
-  -- Auto pairs
-  {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-    require("nvim-autopairs").setup({})
-    end
-  },
-
-  -- FZF
-  {
-    "nvim-telescope/telescope.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
-        cond = function()
-        return vim.fn.executable("make") == 1
-        end,
-      },
+    {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = function()
+      require("nvim-autopairs").setup({})
+      end
     },
-    config = function()
-    local telescope = require("telescope")
-    telescope.setup({
-      defaults = {
-        prompt_prefix = "🔍 ",
-        selection_caret = " ",
-        sorting_strategy = "ascending",
-        layout_config = { prompt_position = "top" },
+
+    {
+      "nvim-telescope/telescope.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+          "nvim-telescope/telescope-fzf-native.nvim",
+          build = "make",
+          cond = function() return vim.fn.executable("make") == 1 end,
+        },
       },
-    })
+      config = function()
+      local telescope = require("telescope")
+      telescope.setup({
+        defaults = {
+          prompt_prefix = "🔍 ",
+          selection_caret = " ",
+          sorting_strategy = "ascending",
+          layout_config = { prompt_position = "top" },
+        },
+      })
+      pcall(telescope.load_extension, "fzf")
 
-  pcall(telescope.load_extension, "fzf")
+      vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
+      vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", { desc = "Buffers" })
+      end
+    },
 
-  -- FZF keymaps
-  vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", { desc = "Find files" })
-  vim.keymap.set("n", "<leader>g", "<cmd>Telescope live_grep<CR>", { desc = "Live grep" })
-  vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>", { desc = "Buffers" })
-  end
-  },
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+      require("lualine").setup({
+        options = {
+          theme = "everforest", -- you can change this to "gruber-darker" if you prefer
+          section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+          icons_enabled = true,
+          globalstatus = true,
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
+        },
+        extensions = { "nvim-tree" },
+      })
+      end
+    },
+
+    -- New theme agine u can put any theme u want
+    {
+      "morhetz/gruvbox",
+      lazy = true,
+      config = function()
+      vim.cmd("colorscheme gruber-darker")
+      end
+    },
+
   })
+
